cf95c8aec356b0311df85fb651ef7d3603a5d50f
Alexander Butakov <asbutakov@gmail.com>
2021-12-15 19:08:22 +0300

    create Recipe, RecipeTags, RecipeIngredients, FavoriteRecipe models register in admin panel

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,7 +1,12 @@
 from django.conf import settings
 from django.contrib import admin
 
-from .models import Tag, Ingredient
+from .models import (Tag,
+                     Ingredient,
+                     Recipe,
+                     RecipeTags,
+                     RecipeIngredients,
+                     FavoriteRecipe)
 
 
 class SiteAdmin(admin.ModelAdmin):
@@ -18,8 +23,43 @@ class TagsAdmin(SiteAdmin):
 
 
 @admin.register(Ingredient)
-class IngredientSerializer(SiteAdmin):
+class IngredientAdmin(SiteAdmin):
     list_display = ('id',
                     'name',
                     'measurement_unit',)
     search_fields = ('name',)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(SiteAdmin):
+    list_display = ('id',
+                    'name',
+                    'author',
+                    'in_favorite',)
+    list_filter = ('name', 'author', 'in_favorite')
+
+    # def in_favorite(self, obj):
+    #     return obj.in_favorite.all().count()
+
+    @staticmethod
+    def in_favorite(obj):
+        return obj.in_favorite.all().count()
+
+
+class RecipeIngredientsInline(admin.TabularInline):
+    model = RecipeIngredients
+    min_num = 1
+    extra = 1
+
+
+class RecipeTagsInline(admin.TabularInline):
+    model = RecipeTags
+    min_num = 1
+    extra = 0
+
+
+@admin.register(FavoriteRecipe)
+class FavoriteRecipeAdmin(SiteAdmin):
+    list_display = ('id',
+                    'user',
+                    'recipe',)
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,7 +1,11 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from .fields import HexColorField
+
+User = get_user_model()
 
 
 class Tag(models.Model):
@@ -38,5 +42,104 @@ class Ingredient(models.Model):
         return self.name
 
     class Meta:
-        ordering = ('id',)
+        ordering = ('name',)
         verbose_name_plural = 'Ингредиенты'
+        constraints = (
+            models.UniqueConstraint(fields=('name',
+                                            'measurement_unit'),
+                                    name='pair_unique'),
+        )
+
+
+class Recipe(models.Model):
+    name = models.CharField(verbose_name=_('Название'),
+                            max_length=200, )
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='recipes',
+                               verbose_name=_('Автор рецепта'))
+    text = models.TextField(verbose_name=_('Описание'))
+    tags = models.ManyToManyField(
+        Tag,
+        through='RecipeTags',
+        verbose_name=_('Идентификаторы рецепта')
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='RecipeIngredients',
+        verbose_name='Ингредиенты рецепта'
+    )
+    image = models.ImageField(upload_to='recipes/')
+    cooking_time = models.PositiveSmallIntegerField(
+        validators=(MinValueValidator(1),),
+        verbose_name=_('Время готовки'))
+
+    REQUIRED_FIELDS = '__all__'
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'recipes'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name
+
+
+class RecipeTags(models.Model):
+    recipe = models.ForeignKey(Recipe,
+                               on_delete=models.CASCADE,
+                               verbose_name=_('Идентификатор рецептов'),)
+    tag = models.ForeignKey(Tag,
+                            on_delete=models.CASCADE,
+                            verbose_name=_('Идентификатор'))
+
+    class Meta:
+        verbose_name = 'Tags'
+        verbose_name_plural = 'Идентификаторы'
+
+    def __str__(self):
+        return (self.tag_id.name[:15] + '-->' + self.recipe_id.name[:15])
+
+    def __repr__(self):
+        return (self.tag_id.name[:15] + '-->' + self.recipe_id.name[:15])
+
+
+class RecipeIngredients(models.Model):
+    recipe = models.ForeignKey(Recipe,
+                               on_delete=models.CASCADE,
+                               verbose_name=_('Ингредиент рецептов'))
+    ingredient = models.ForeignKey(Ingredient,
+                                   on_delete=models.PROTECT,
+                                   verbose_name=_('Ингредиент'))
+    amount = models.PositiveSmallIntegerField(
+        validators=(MinValueValidator(1),),
+        verbose_name=_('Время приготовления'),
+    )
+
+    class Meta:
+        verbose_name = 'Ingredients'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return (self.ingredient_id.name[:15] + 'в' + self.recipe_id.name[:15])
+
+
+class FavoriteRecipe(models.Model):
+    recipe = models.ForeignKey(Recipe,
+                               on_delete=models.CASCADE,
+                               verbose_name=_('Рецепт'),
+                               related_name='in_favorite')
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE,
+                             verbose_name=_('Пользователь'),
+                             related_name='favorite')
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(fields=('recipe',
+                                            'user'),
+                                    name='pair_unique'),
+        )
+        ordering = ('-id',)
+        verbose_name = 'Favorite'
+        verbose_name_plural = 'Избранные рецепты'

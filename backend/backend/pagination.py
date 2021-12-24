e7c7687b1d719d116b7323ae12a138cb013a89ee
Alexander <85052532+K3llar@users.noreply.github.com>
2021-12-24 18:48:45 +0300

    Update pagination.py

--- backend/backend/pagination.py
+++ backend/backend/pagination.py
@@ -3,4 +3,3 @@ from rest_framework.pagination import PageNumberPagination
 
 class CustomPageNumberPaginator(PageNumberPagination):
     page_size_query_param = 'limit'
-    # page_size_query_param = 6

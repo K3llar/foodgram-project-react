6cb494b780ebc540942f31a70334f6010a06887c
Alexander Butakov <asbutakov@gmail.com>
2021-12-28 19:25:25 +0300

    add default settings to database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -79,11 +79,16 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE',
                             default='django.db.backends.postgresql'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'NAME': os.environ.get('DB_NAME',
+                               default='postgres'),
+        'USER': os.environ.get('POSTGRES_USER',
+                               default='postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD',
+                                   default='postgres'),
+        'HOST': os.environ.get('DB_HOST',
+                               default='db'),
+        'PORT': os.environ.get('DB_PORT',
+                               default=5432),
     }
 }
 
